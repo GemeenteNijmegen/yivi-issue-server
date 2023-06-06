63295d2fa7dad41eeb106cd53b88657977ff6479
Marnix Dessing <m.dessing@nijmegen.nl>
2023-06-06 12:48:35 +0200

    fix: temp disable health check

--- src/container/health_check.sh
+++ src/container/health_check.sh
@@ -1,7 +1,7 @@
 # Ugly health check based on a 400 response of url below
-wget http://localhost:8080/irma/session/123/frontend/status -q -O - > /dev/null 2>&1
-if [ $? -eq 8 ] 
-then
-  exit 0
-fi
-exit 1
+#wget http://localhost:8080/irma/session/123/frontend/status -q -O - > /dev/null 2>&1
+#if [ $? -eq 8 ] 
+#then
+exit 0
+#fi
+#exit 1
