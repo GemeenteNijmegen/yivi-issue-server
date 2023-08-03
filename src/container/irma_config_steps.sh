b5c9a0b25204e1d36f4beafde278b9a2507ecec0
Marnix Dessing <m.dessing@nijmegen.nl>
2023-08-03 13:37:09 +0200

    fix: make directories at runtime and copy files

--- src/container/irma_config_steps.sh
+++ src/container/irma_config_steps.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+echo "Setting up irma directory"
+mkdir /storage/irma
+
 echo "Updating irma_config.json file"
-echo $IRMA_GW_URL
+cp /tmp/irma_config.json /storage/irma/irma_config.json
 sed -i -e "s/IRMA_TOKEN/$IRMA_TOKEN/g" /storage/irma/irma_config.json
 sed -i -e "s/IRMA_GW_URL/https:\/\/$IRMA_GW_URL/g" /storage/irma/irma_config.json
 
+mkdir /storage/irma/privatekeys
 echo "$IRMA_GEMEENTE_PRIVKEY" > /storage/irma/privatekeys/pbdf.gemeente.xml
 
 echo "Starting irma server with custom config file"
